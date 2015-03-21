12dc702caf9a98ed24b29abc2de7756c7811dcf6
everaldomatias <matiaseveraldo@gmail.com>
2015-03-21 13:56:03 -0300

    Ajustes no header interno, aplica menu default e adiciona link no logo para a home

--- header.php
+++ header.php
@@ -23,24 +23,36 @@ $options = get_option('geral_cfg');
 </head>
 
 <body <?php body_class(); ?>>
-	<nav id="menu-top" class="col-md-12 barra-bg">
+	<nav id="menu-top" class="col-md-12 barra-bg internal-menu">
 		<div class="container">
 			<div class="row">
 				<div class="pull-left">
-					<?php echo wp_get_attachment_image($options['logo'],'full');?>
+					<a href="<?php echo home_url(); ?>">
+						<?php echo wp_get_attachment_image( $options['logo'],'full' );?>
+					</a>
 				</div><!-- .pull-left -->
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'main-menu',
-						'depth'          => 2,
-						'container'      => false,
-						'menu_class'     => 'nav navbar-nav',
-						'fallback_cb'    => 'Odin_Bootstrap_Nav_Walker::fallback',
-						'walker'         => new Odin_Bootstrap_Nav_Walker()
-					)
-				);
-				?>
+				<?php if ( has_nav_menu( 'main-menu' ) ) : ?>
+					<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'main-menu',
+								'depth'          => 2,
+								'container'      => false,
+								'menu_class'     => 'nav navbar-nav',
+								'fallback_cb'    => 'Odin_Bootstrap_Nav_Walker::fallback',
+								'walker'         => new Odin_Bootstrap_Nav_Walker()
+							)
+						);
+					?>
+				<?php else : ?>
+					<?php if ( is_user_logged_in() && current_user_can( 'administrator' ) ) : ?>
+						<a class="css-cor" href="<?php echo admin_url( 'nav-menus.php' ); ?>"><?php _e( 'Create your first menu', 'odin' ); ?></a>
+					<?php else : ?>
+						<ul class="default-menu">
+							<?php echo wp_list_pages( 'title_li=' ); ?>
+						</ul><!-- default-menu -->
+					<?php endif; ?>
+				<?php endif; ?>
 			</div><!-- .row -->
 		</div><!-- .container -->
 	</nav><!-- #menu-top.col-md-12 -->
