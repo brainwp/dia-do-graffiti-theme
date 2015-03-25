05e4b38cc234bfddea5626856547baa35498d9b9
everaldomatias <matiaseveraldo@gmail.com>
2015-03-25 08:36:05 -0300

    Ajustes Footer

--- footer.php
+++ footer.php
@@ -15,20 +15,26 @@
 			<div class="col-md-4 pull-left">
 				<div class="col-md-12">
 					<?php _e('Alguns direitos reservados','odin');?>
-					<img src="<?php bloginfo('template_url');?>/assets/images/cc-icon.jpg">
+					<img src="<?php bloginfo('template_url');?>/assets/images/cc-icon.png">
 				</div><!-- .col-md-12 -->
 				<div class="col-md-12">
 					<?php _e('Desenvolvido com','odin');?>
-					<img src="<?php bloginfo('template_url');?>/assets/images/wp-icon.jpg">
+					<img src="<?php bloginfo('template_url');?>/assets/images/wp-icon.png">
 				</div><!-- .col-md-12 -->
 			</div><!-- .col-md-4 -->
-			<div class="col-md-8 pull-right">
-				<a target="_blank" class="pull-right" href="http://brasa.art.br">
-					<img alt="Brasa" src="<?php bloginfo('template_url');?>/assets/images/brasa.jpg">
-				</a>
-				<a target="_blank" class="pull-right" href="http://www.acaoeducativa.org/">
-					<img alt="Ação Educativa" src="<?php bloginfo('template_url');?>/assets/images/acaoeducativa.jpg">
-				</a>
+			<div class="col-md-8 pull-right logos">
+
+				<div class="col-sm-12 pull-right">
+				
+					<a target="_blank" href="http://brasa.art.br">
+						<img alt="Brasa" src="<?php bloginfo('template_url');?>/assets/images/brasa.png">
+					</a>
+					<a target="_blank" href="http://www.acaoeducativa.org/">
+						<img alt="Ação Educativa" src="<?php bloginfo('template_url');?>/assets/images/acaoeducativa.png">
+					</a>
+
+				</div>
+
 			</div><!-- .col-md-8 pull-right -->
 		</div><!-- .row -->
 	</div><!-- .container -->
