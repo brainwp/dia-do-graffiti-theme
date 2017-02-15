54380b53ff89f87f1bef0d74e718bf04479591e6
everaldomatias <matiaseveraldo@gmail.com>
2017-02-15 19:26:10 -0200

    Remove funcao depreciada kirki_get_option

--- parts/links-home.php
+++ parts/links-home.php
@@ -1,19 +1,19 @@
 <?php
-/* Template name: Links home */
+/* Template name: Links Home */
 ?>
 <section id="links-home" class="col-sm-3 links-rapidos">
-	<?php if( $title = kirki_get_option( 'fast_link_1_title' ) ) : ?>
-		<a class="bg-cor" href="<?php echo esc_url( kirki_get_option( 'fast_link_1' ) ); ?>">
+	<?php if( $title = get_theme_mod( 'fast_link_1_title' ) ) : ?>
+		<a class="bg-cor" href="<?php echo esc_url( get_theme_mod( 'fast_link_1' ) ); ?>">
 			<?php echo esc_textarea( $title ); ?>
 		</a>
 	<?php endif; ?>
-	<?php if( $title = kirki_get_option( 'fast_link_2_title' ) ) : ?>
-		<a class="bg-cor" href="<?php echo esc_url( kirki_get_option( 'fast_link_2' ) ); ?>">
+	<?php if( $title = get_theme_mod( 'fast_link_2_title' ) ) : ?>
+		<a class="bg-cor" href="<?php echo esc_url( get_theme_mod( 'fast_link_2' ) ); ?>">
 			<?php echo esc_textarea( $title ); ?>
 		</a>
 	<?php endif; ?>
-	<?php if( $title = kirki_get_option( 'fast_link_3_title' ) ) : ?>
-		<a class="bg-cor" href="<?php echo esc_url( kirki_get_option( 'fast_link_3' ) ); ?>">
+	<?php if( $title = get_theme_mod( 'fast_link_3_title' ) ) : ?>
+		<a class="bg-cor" href="<?php echo esc_url( get_theme_mod( 'fast_link_3' ) ); ?>">
 			<?php echo esc_textarea( $title ); ?>
 		</a>
 	<?php endif; ?>
